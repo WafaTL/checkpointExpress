75f4edef267626273a2cee7c72f25a7f067a00bd
WafaTL <wafatliliing@gmail.com>
2023-02-26 20:27:22 +0100

    first commit

---
+++ middelwares/myLogger.js
@@ -0,0 +1,8 @@
+const myLogger = function (req, res, next) {
+    // const date = new Date();
+    // const day = date.getDay();
+    // const hour = date.getHours();
+    // if ((day >=6)||(day<1) ||(hour<9 ||hour>17) ) { res.send('closed');
+    // } else next();
+    }
+module.exports=myLogger;
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path=require('path')
+const app = express();
+
+
+// app.use(myLogger);
+
+app.use(express.static(path.join(__dirname, 'public')));
+    app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    });
+    app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+        });
+    app.get('/service', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'service.html'));
+            });
+        
+
+    
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Server started on port ${PORT}'));
